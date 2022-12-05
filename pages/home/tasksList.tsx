b9619afc2628603ab75c341ab3f02139136ba3b1
xtawfeeq7k <tawfeeqkhalilia51@gmail.com>
2022-12-05 21:16:59 +0200

    change the theme and fix the build issues

--- pages/home/tasksList.tsx
+++ pages/home/tasksList.tsx
@@ -1,31 +1,27 @@
-import React, { useReducer, useState } from "react";
+import React from "react";
 
-const TasksList = (tasks: never[]) => {
-  let i: number = 0;
-  let j: number = 0;
-
-  return tasks.map((task: string) => (
+const deleteTask = (task: any) => {
+  let tasks1: any = localStorage.getItem("tasks");
+  const tasks = tasks1 ? JSON.parse(tasks1.toString()) : [];
+  const tsks = [];
+  for (let t of tasks) {
+    if (t !== task) tsks.push(t);
+  }
+  localStorage.setItem("tasks", JSON.stringify(tsks));
+  window.location.reload();
+};
+const TasksList = (tasks: never[]) =>
+  Object.keys(tasks).map((task: any) => (
     <>
       <h4
-        key={j++}
-        id={"task" + i++}
+        key={task}
+        id={task}
         onClick={() => {
-          let tasks1: any = localStorage.getItem("tasks");
-          const tasks = tasks1 ? JSON.parse(tasks1.toString()) : [];
-
-          const tsks = [];
-
-          for (let t of tasks) {
-            if (t !== task) tsks.push(t);
-          }
-          localStorage.setItem("tasks", JSON.stringify(tsks));
-          window.location.reload();
+          deleteTask(tasks.at(task));
         }}
       >
-        {task}
+        {tasks.at(task)}
       </h4>
     </>
   ));
-};
-
 export default TasksList;
